02bc7269428105350b82069d130cfcfafffdf851
adokl <762343364@qq.com>
2022-10-12 18:10:13 +0800

    实验

--- app/src/main/java/com/example/myapplication/Fragment_friend.java
+++ app/src/main/java/com/example/myapplication/Fragment_friend.java
@@ -1,18 +1,23 @@
-package com.example.myapplication;
+package com.example.myapplication;import android.app.Fragment;
 import android.content.Context;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import androidx.fragment.app.Fragment;
+
+import androidx.annotation.RequiresApi;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import java.util.ArrayList;
 import java.util.List;
+
 public class Fragment_friend extends Fragment {
     private RecyclerView recyclerView;
     private Adapter adapter;
     private List<String> list;
+
+    @RequiresApi(api = Build.VERSION_CODES.M)
     @Override
     public View onCreateView(LayoutInflater inflater,ViewGroup container, Bundle savedInstanceState) {
         View view;
